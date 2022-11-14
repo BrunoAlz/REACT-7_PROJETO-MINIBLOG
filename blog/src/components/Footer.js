d073f0b3bde96abfb081892049349dcc7c21f096
Bruno Alves <bruno6821@gmail.com>
2022-11-13 21:03:32 -0300

    Configurando o Footer

--- blog/src/components/Footer.js
+++ blog/src/components/Footer.js
@@ -1,7 +1,12 @@
+import styles from "./Footer.modules.css";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <h3>Escreva sobre o que você tem Interesse!</h3>
+      <p>Mini Blog - REACT &copy; 2022</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
