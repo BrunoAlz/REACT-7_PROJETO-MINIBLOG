bf0e1f9b9fab754d4f9e2a71abfa36e2cb87e72c
Bruno Alves <bruno6821@gmail.com>
2022-11-13 21:10:31 -0300

    Implementando o CSS do Footer

--- blog/src/components/Footer.js
+++ blog/src/components/Footer.js
@@ -1,8 +1,8 @@
-import styles from "./Footer.modules.css";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre o que você tem Interesse!</h3>
       <p>Mini Blog - REACT &copy; 2022</p>
     </footer>
