120e0560e9ea524cd5df9a841a0e354ab40ca0b5
Bruno Alves <bruno6821@gmail.com>
2022-11-14 13:19:03 -0300

    Implementando o JSX do Registro de Usuários

--- blog/src/pages/Register/Register.js
+++ blog/src/pages/Register/Register.js
@@ -1,8 +1,51 @@
 import styles from "./Register.module.css";
+
+import { useState, useEffect } from "react";
+
 const Register = () => {
   return (
     <div>
-      <h2>Cadastrar</h2>
+      <h1>Cadastre-se para postar!</h1>
+      <p>Crie seu usuário e compartilhe suas Histórias!</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua Senha"
+          />
+        </label>
+        <label>
+          <span>Confirme a Senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme sua Senha"
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
     </div>
   );
 };
