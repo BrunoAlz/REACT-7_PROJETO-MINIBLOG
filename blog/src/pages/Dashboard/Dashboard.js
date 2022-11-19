95ba43b604b63ba69fc2a6432cb9e8f0ac879805
Bruno Alves <bruno6821@gmail.com>
2022-11-19 13:41:57 -0300

    Implementação inicial do Dashboard de Posts

--- blog/src/pages/Dashboard/Dashboard.js
+++ blog/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,33 @@
-import styles from './Dashboard.module.css'
+import styles from "./Dashboard.module.css";
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
-  return (
-    <div><h1>Dashboard</h1></div>
-  )
-}
+  const { user } = useAuthValue();
+  const uid = user.uid;
 
-export default Dashboard
+  const posts = [];
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Gerencie seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/post/create" className="btn">
+            Criar Primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem post</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
