f40eabf8bfda7dfb9384ccc7d36003c601fb14e0
Bruno Alves <bruno6821@gmail.com>
2022-11-20 11:59:38 -0300

    Implementando a Funcionalidade de Deleção de POSTS

--- blog/src/pages/Dashboard/Dashboard.js
+++ blog/src/pages/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
@@ -14,7 +15,7 @@ const Dashboard = () => {
     error,
   } = useFetchDocuments("posts", null, uid);
 
-  const deleteDocument = (id) => {};
+  const { deleteDocument } = useDeleteDocument("posts");
 
   if (loading) {
     return <p>Carregando...</p>;
